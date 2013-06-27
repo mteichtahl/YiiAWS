4eeb34c033e24cb7751c1686d7d33efec56b0456
Charles Pick <charles.pick@gmail.com>
2013-06-27 01:00:02 +0100

    Restructure for composer

--- src/YiiAWS/AppComponent.php
+++ src/YiiAWS/AppComponent.php
@@ -4,32 +4,27 @@
  * YiiAWS Component
  *
  * This is a Yii component (CApplicationComponent)
- * 
+ *
  *  @author Marc Teichtahl <marc@teichtahl.com>
  * @copyright Copyright &copy; Marc Teichtahl 2013-
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @version 1.0.0
  * @see http://docs.aws.amazon.com/aws-sdk-php-2/latest/
- * 
+ *
  * @package YiiAMQP
  **/
-require __DIR__.'/aws.phar';
-
-Yii::app()->autoloader->getAutoloader()->addNamespace('Aws\S3', __DIR__ . '/aws-sdk/src/Aws/S3');
-Yii::app()->autoloader->getAutoloader()->addNamespace('Aws\S3\Exception', __DIR__ . '/aws-sdk/src/Aws/S3/Exception');
-Yii::app()->autoloader->getAutoloader()->addNamespace('Aws\S3\Enum', __DIR__ . '/aws-sdk/src/Aws/S3/Enum');
-
+namespace YiiAWS;
 
 /**
  * YiiAWS
  *
  * The class is a Yii CApplicationCompontent to be used as a component in Yii applications.
- * 
+ *
  *
  * @package    YiiAWS
  * @subpackage Common
  * @author     Marc Teichtahl <marc@teichtahl.com>
- * 
+ *
  * /**
  * Client to interact with Amazon Simple Storage Solution
  *
@@ -88,49 +83,81 @@ Yii::app()->autoloader->getAutoloader()->addNamespace('Aws\S3\Enum', __DIR__ . '
  */
 
 
-/**
- * $poo = app()->YiiAWS->init();
-//
-//$result = $poo->listBuckets();
-//
-//foreach ($result['Buckets'] as $bucket) {
-//    echo "- {$bucket['Name']}<br>";
-//
-//    $listObjectsCommand = $poo->getCommand('ListObjects', array('Bucket' => 'jg_511841c60a6f7'));
-//
-//    $iterator = $poo->getIterator($listObjectsCommand);
-//
-//    foreach ($iterator as $object) {
-//        echo $object['Key'] . ' - ' . $object['Size'] . 'Bytes - ' . $object['LastModified'] . '</br>';
-//    }
-//}
- */
-class YiiAWS extends CApplicationComponent {
+class AppComponent extends \CApplicationComponent
+{
 
-    public $key;
-    public $secret;
-    public $region;
-    protected $aws = NULL;
-    protected $config = array();
+    /**
+     * @var array the configuration array, should have key, secret and region keys.
+     */
+    public $config = array();
 
-    public function init() {
-        $this->config = array(
-            'key' => $this->key,
-            'secret' => $this->secret,
-            'region' => $this->region
-        );
+    /**
+     * @var \Aws\S3\S3Client
+     */
+    protected $_client;
 
-        $this->aws = Aws\S3\S3Client::factory($this->config);
-
-        // Check that the factory created our object
-        if (get_class($this->aws) != 'Aws\S3\S3Client')
-        {
-            // We couldnt create it so throw an exception
-            throw new CHttpException(404,'Could not create AWS Service.');
-        }        
-        return Aws\S3\S3Client::$this->aws;
+    /**
+     * @param \Aws\S3\S3Client $client
+     */
+    public function setClient($client)
+    {
+        $this->_client = $client;
     }
 
-}
+    /**
+     * @return \Aws\S3\S3Client
+     */
+    public function getClient()
+    {
+        if ($this->_client === null)
+            $this->_client = \Aws\S3\S3Client::factory($this->config);
+        return $this->_client;
+    }
 
-?>
+    public function __call($name, $arguments)
+    {
+        $client = $this->getClient();
+        if (method_exists($client, $name))
+            return call_user_func_array(array($client, $name), $arguments);
+        else
+            throw new \Exception(__CLASS__." does not have a method called '$name'");
+    }
+
+    public function __get($name)
+    {
+        $client = $this->getClient();
+        if (property_exists($client, $name))
+            return $client->{$name};
+        else
+            return parent::__get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        $client = $this->getClient();
+        if (property_exists($client, $name))
+            return $client->{$name} = $value;
+        else
+            return parent::__set($name, $value);
+    }
+
+    public function __isset($name)
+    {
+        $client = $this->getClient();
+        if (property_exists($client, $name))
+            return isset($client->{$name});
+        else
+            return parent::__isset($name);
+    }
+
+    public function __unset($name)
+    {
+        $client = $this->getClient();
+        if (property_exists($client, $name))
+            unset($client->{$name});
+        else
+            parent::__unset($name);
+    }
+
+
+}
